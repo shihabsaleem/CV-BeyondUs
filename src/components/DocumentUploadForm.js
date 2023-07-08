603c6a05e300c2c4336cfedf4a87d11fd24cd8da
shihabsaleem <shihabchikkushihab@gmail.com>
2023-07-08 19:07:26 +0530

    upload- dummy

--- src/components/DocumentUploadForm.js
+++ src/components/DocumentUploadForm.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import "./DocumentUploadForm.css"
+import React, { useState } from "react";
+import { Card, CardContent, Typography, Button } from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import "./DocumentUploadForm.css";
 
 const DocumentUploadForm = () => {
   const [selectedFile, setSelectedFile] = useState(null);
@@ -12,37 +14,65 @@ const DocumentUploadForm = () => {
     event.preventDefault();
     // Perform the file upload here
     if (selectedFile) {
-      console.log('Uploading file:', selectedFile);
+      console.log("Uploading file:", selectedFile);
       // You can use APIs like fetch or axios to send the file to the server
       // Example using fetch:
       const formData = new FormData();
-      formData.append('document', selectedFile);
-      fetch('/upload', {
-        method: 'POST',
+      formData.append("document", selectedFile);
+      fetch("/upload", {
+        method: "POST",
         body: formData,
       })
         .then((response) => {
           // Handle the response from the server
-          console.log('Upload successful:', response);
+          console.log("Upload successful:", response);
         })
         .catch((error) => {
           // Handle errors
-          console.error('Upload failed:', error);
+          console.error("Upload failed:", error);
         });
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="document">Select a document to upload:</label>
-      <input
-        type="file"
-        id="document"
-        accept=".pdf,.doc,.docx"
-        onChange={handleFileChange}
-      />
-      <button type="submit">Upload</button>
-    </form>
+    <Card variant="outlined" className="document-upload-card">
+      <CardContent>
+        <Typography variant="h5" component="h2" gutterBottom>
+          Upload a Document
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="file"
+            accept=".pdf,.doc,.docx"
+            onChange={handleFileChange}
+            id="contained-button-file"
+            style={{ display: "none" }}
+          />
+          <label htmlFor="contained-button-file">
+            <Button
+              variant="contained"
+              component="span"
+              startIcon={<CloudUploadIcon />}
+            >
+              Select Document
+            </Button>
+          </label>
+          {selectedFile && (
+            <Typography variant="body1" gutterBottom>
+              Selected File: {selectedFile.name}
+            </Typography>
+          )}
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={!selectedFile}
+            startIcon={<CloudUploadIcon />}
+          >
+            Upload
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
   );
 };
 
